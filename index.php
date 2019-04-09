959aadfd4d1f5d02d622242a0c319357740b3567
Inderdeep Singh Bajwa <InderdeepBajwa@users.noreply.github.com>
2019-04-09 01:54:56 -0400

    Login handling implementation

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
 
-
-<ul>
-  <li><a href="default.asp">Home</a></li>
-  <li><a href="news.asp">News</a></li>
-  <li><a href="contact.asp">Contact</a></li>
-  <li><a href="about.asp">About</a></li>
-</ul>
-
-<style>
-<?php include 'assets/css/index.css'; ?>
-</style>
-=======
 <?php
-
-    
-
+  require('partials/nav.php')
 
 
 ?>
->>>>>>> f200887054864928825fa2e74b3deb62c5c46685
---
+++ partials/nav.php
@@ -0,0 +1,8 @@
+<?php require('head.php'); ?>
+
+<ul>
+  <li><a href="/jig">Finance Management</a></li>
+  <li><a class="right-align" href="#">Contact</a></li>
+  <li><a class="right-align" href="#">About</a></li>
+  <li><a class="right-align" href="/jig/pages/login.php">Login</a></li>
+</ul>
